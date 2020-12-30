25e3803abe252e740a77f306a250056722604e7e
Paul <boisson.paul@yahoo.fr>
2020-12-30 22:31:55 +0100

    Rédaction du Readme,ajout des frameworks et librairie exigées, finalisation de l'interface web, ajout d'un script de test.

--- main.py
+++ main.py
@@ -5,67 +5,70 @@ from flask import Flask, flash, request, redirect, url_for, render_template, sen
 from werkzeug.utils import secure_filename
 
 from PIL import Image
-from PIL.ExifTags import TAGS
 
 import json
 
-# Cette variable n'est pas un paramètre de l'application car est utilisée dans allowed_file. 
+
+# Fonction de sécurisation des images
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Première app.route qui permet de lancer la page html lorsqu'on lance le serveur et qu'on s'y connecte. S'y connecter, c'est une requête GET. 
-@app.route('/', methods=['GET'])
-def upload_form():
-	files = os.listdir(app.config['UPLOAD_FOLDER']) # files devient la liste des fichiers dans le dossier d'upload. 
-	return render_template('upload.html', files=files) # On renvoit la page html contenant la liste des fichiers dans le dossier d'upload
 
-# Lorsqu'on appuie sur le bouton submit de la page HTML, celui-ci émet une requête POST. 
+# Route de connection à l'application (affichage des images déjà chargées)
+@app.route('/', methods=['GET'])
+def webPage():
+	files = os.listdir(app.config['UPLOAD_FOLDER']) 
+	return render_template('upload.html', files=files) 
+
+# Route de chargement d'une image
 @app.route('/', methods=['POST'])
 def upload_image():
 	if 'file' not in request.files:
-		flash('No file part')	# Message flash
+		flash('No file part')
 		return redirect(request.url)
-	file = request.files['file'] # file devient le fichier chargé dans la page html
+	file = request.files['file'] 
+	if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], file.filename)):
+		flash('Image déjà chargée')
+		return redirect(request.url)
 	if file.filename == '':
-		flash('No image selected for uploading') # Message flash
+		flash('Aucune image sélectionnée')
 		return redirect(request.url)
 	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename) # Il faut sécuriser le nom du fichier pour éviter que des commandes se cachent dedans.  
-
-		# ICI IL FAUT RESIZE L'IMAGE
-		
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # Ce n'est qu'à partir du moment où on utilise la méthode save que le fichier est enregistré sur le disque 
-		flash('Image successfully uploaded and displayed') # Le flash est un élément qui peut être récupéré par la partie html avec jinja2. 
+		filename = secure_filename(file.filename)
+		img=Image.open(file)
+		print('La taille de l image est :', img.size)
+		img.thumbnail((400, 400)) # Redimensionnement de l'image
+		print('Maintenant la taille de l image est :',img.size)
+		img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
+		flash('Image correctement chargée et affichée') 
 		return redirect(request.url)
 	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		flash('Ce format d image n est pas autorisé. Veuiller utiliser les formats suivants: png, jpg, jpeg, gif')
 		return redirect(request.url)
 
-# Cette fonction est déclenchée par l'affichage d'une image dans la partie html. 
+# Route pour afficher un des fichier en grand
 @app.route('/display/<filename>',methods=['GET'])
 def display_image(filename):
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
-	#return redirect(request.url)
 
-@app.route('/uploadedFiles')
-def show_list():
-	return [os.listdir(app.config['UPLOAD_FOLDER'])]
-
+# Route pour supprimer un fichier du stockage
 @app.route('/removeFile/<filename>')
 def removeFile(filename):
 	try:
    		os.remove("./static/uploads/"+filename)
 	except:
 		flash('le fichier n existe pas')
-		return redirect(url_for('upload_form'))
+		return redirect(url_for('webPage'))
 	else:
-		return redirect(url_for('upload_form'))
+		return redirect(url_for('webPage'))
 
+# Route pour obtenir les metadonnées d'une des images
 @app.route('/metadata/<filename>')
 def seeMetadata(filename):
 	img = Image.open('./static/uploads/'+ filename)
-	return(json.dumps({'filename':filename, 'format':img.format, 'size':img.size},indent=4))
+	return(json.dumps({'Nom':filename.rsplit('.', 1)[0].lower(), 'Format':img.format, 'Dimensions':img.size},indent=4))
 
+# Lancement de l'application à l'execution du script
 if __name__ == "__main__":
     app.run(debug=True)
